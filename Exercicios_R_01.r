26a2bd0133eb99a83a7bb2f75076cbc905300bbd
Natalia-S-Pereira <73372366+Natalia-S-Pereira@users.noreply.github.com>
2021-03-22 17:36:42 -0300

    Update Exercicios_R_01.r

--- Exercicios_R_01.r
+++ Exercicios_R_01.r
@@ -1,3 +1,4 @@
+# 1
 Ana = 8L;
 Paulo = 12L;
 Ana
@@ -8,9 +9,11 @@ class(Paulo)
 mais_velho= ifelse (Ana > Paulo, "A menina é mais velha", "A menino é mais velho")
 mais_velho
 
+# 2
 bod= class(BOD)
 bod
 
+# 3
 x= c(1,2,3,4,5,6,7,8,9,10)
 
 for (i in 1:10){
@@ -19,17 +22,20 @@ for (i in 1:10){
 }
 }
 
-
+# 4
 ultimas_linhas= tail(women, 10)
 ultimas_linhas
 
+#5
 novairis = iris[50:100,c(2,3)]
 plot(novairis)
 
+#6
 x= c(2,4,6,8,10)
 y= c(1,3,5,7,9)
 print(x+y)
 
+#7
 dim(CO2)[1]
 dim(CO2)[2]
 rownames(CO2)
