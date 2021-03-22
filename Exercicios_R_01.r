f2b0345a2badafd334a1dcf1d89eb4e4ce1d3486
Natalia-S-Pereira <73372366+Natalia-S-Pereira@users.noreply.github.com>
2021-03-22 17:23:11 -0300

    Add files via upload

---
+++ Exercicios_R_01.r
@@ -0,0 +1,38 @@
+Ana = 8L;
+Paulo = 12L;
+Ana
+class(Ana)
+Paulo
+class(Paulo)
+
+mais_velho= ifelse (Ana > Paulo, "A menina é mais velha", "A menino é mais velho")
+mais_velho
+
+bod= class(BOD)
+bod
+
+x= c(1,2,3,4,5,6,7,8,9,10)
+
+for (i in 1:10){
+    if (x[i]%% 2==0){
+    print(x[i])
+}
+}
+
+
+ultimas_linhas= tail(women, 10)
+ultimas_linhas
+
+novairis = iris[50:100,c(2,3)]
+plot(novairis)
+
+x= c(2,4,6,8,10)
+y= c(1,3,5,7,9)
+print(x+y)
+
+dim(CO2)[1]
+dim(CO2)[2]
+rownames(CO2)
+colnames(CO2)
+
+
